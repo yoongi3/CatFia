d04e675ddbe0062a4f21aeec49d7e8593d5c1c83
Josh <lolhongy3@gmail.com>
2024-02-21 17:20:42 +1100

    backend file organisation

---
+++ BackEnd/dist/sockets/socketServer.js
@@ -0,0 +1,31 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const socket_io_1 = require("socket.io");
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+const server = http_1.default.createServer(app);
+app.use((0, cors_1.default)());
+const io = new socket_io_1.Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    }
+});
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('message', (data) => {
+        console.log('Received test message from frontend:', data);
+        const randomNumber = Math.floor(Math.random() * 100) + 1;
+        socket.emit('randomNumber', randomNumber);
+    });
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+exports.default = server;
+//# sourceMappingURL=socketServer.js.map
--- BackEnd/src/index.tsx
+++ BackEnd/src/index.tsx
@@ -1,34 +1,4 @@
-import express from 'express';
-import http from 'http';
-import { Server } from 'socket.io';
-import cors from 'cors';
-
-const app = express();
-const server = http.createServer(app);
-
-app.use(cors());
-
-const io = new Server(server, {
-  cors: {
-    origin: '*',
-    methods: ['GET', 'POST'], 
-  }
-});
-
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  socket.on('message', (data) => {
-    console.log('Received test message from frontend:', data);
-
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
-    socket.emit('randomNumber', randomNumber);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
-});
+import server from "./sockets/socketServer";
 
 const PORT = process.env.PORT || 5001;
 
